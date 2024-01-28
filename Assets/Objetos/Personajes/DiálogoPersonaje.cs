aa98d16c274e094199480f00b04c7bff222677c5
JuanAndresCoder <151435416+JuanAndresCoder@users.noreply.github.com>
2024-01-28 23:38:45 +0100

    Cámara dinámica
    La cámara ahora se adapta al tamaño de la pantalla
    
    ---COSAS A TERMINAR---
    Animación de conversaciones (Conversar.anim)
    Información de tema (Información de tema.asset)

--- Assets/Objetos/Personajes/DiálogoPersonaje.cs
+++ Assets/Objetos/Personajes/DiálogoPersonaje.cs
@@ -1,6 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.PackageManager;
 using UnityEngine;
 using VikingCrew.Tools.UI;
 public class DiálogoPersonaje : MonoBehaviour
